aaa629dcbcdac1904c152d63a0b5f960c6838aa5
Lalesh <laleshpaudel7@gmail.com>
2022-09-08 17:36:26 +0545

    login.blade.php added

---
+++ resources/views/frontend/login.blade.php
@@ -0,0 +1,126 @@
+
+    <div id="header-hero-container">
+        <header>
+            <nav id="login-page">
+                <div class="flex container">
+                    <a id="logo" href="index.html">Bibhuti Solutions</a>
+                    <nav>
+                        <button id="nav-toggle" class="hamburger-menu">
+                            <i class="fas fa-bars"></i>
+                        </button>
+
+                        <ul id="nav-menu">
+                            <li><a href="index.html" class="active"><b>Home</b></a></li>
+                            <li><a href="about.html"><b>About us</b></a></li>
+                            <li><a href="services.html"><b>Services</b></a></li>
+                            <li><a href="team.html"><b>Team</b></a></li>
+                            <li><a href="blog.html"><b>Blog</b></a></li>
+                            <li><a href="contact.html"><b>Contact us</b></a></li>
+                            <li><a href="login.html"><b>Login</b></a></li>
+                            <li id="close-flyout"><span class="fas fa-window-close"></span></li>
+                        </ul>
+                    </nav>
+                </div>
+            </nav>
+
+        </header>
+
+        <section id="hero">
+            <section class="container forms">
+                <div class="form login">
+                    <div class="form-content">
+                        <header>Login</header>
+                        <form action="#">
+                            <div class="field input-field">
+                                <input type="email" placeholder="Email" class="input">
+                            </div>
+
+                            <div class="field input-field">
+                                <input type="password" placeholder="Password" class="password">
+                                <i class='bx bx-hide eye-icon'></i>
+                            </div>
+
+                            <div class="form-link">
+                                <a href="#" class="forgot-pass">Forgot password?</a>
+                            </div>
+
+                            <div class="field button-field">
+                                <button>Login</button>
+                            </div>
+                        </form>
+
+                        <div class="form-link">
+                            <span>Don't have an account? <a href="#" class="link signup-link">Signup</a></span>
+                        </div>
+                    </div>
+
+                    <div class="line"></div>
+
+                    <div class="media-options">
+                        <a href="#" class="field facebook">
+                            <i class='bx bxl-facebook facebook-icon'></i>
+                            <span>Login with Facebook</span>
+                        </a>
+                    </div>
+
+                    <div class="media-options">
+                        <a href="#" class="field google">
+                            <img src="images/google.png" alt="" class="google-img">
+                            <span>Login with Google</span>
+                        </a>
+                    </div>
+
+                </div>
+
+                <!-- Signup Form -->
+
+                <div class="form signup">
+                    <div class="form-content">
+                        <header>Signup</header>
+                        <form action="#">
+                            <div class="field input-field">
+                                <input type="email" placeholder="Email" class="input">
+                            </div>
+
+                            <div class="field input-field">
+                                <input type="password" placeholder="Create password" class="password">
+                            </div>
+
+                            <div class="field input-field">
+                                <input type="password" placeholder="Confirm password" class="password">
+                                <i class='bx bx-hide eye-icon'></i>
+                            </div>
+
+                            <div class="field button-field">
+                                <button>Signup</button>
+                            </div>
+                        </form>
+
+                        <div class="form-link">
+                            <span>Already have an account? <a href="#" class="link login-link">Login</a></span>
+                        </div>
+                    </div>
+
+                    <div class="line"></div>
+
+                    <div class="media-options">
+                        <a href="#" class="field facebook">
+                            <i class='bx bxl-facebook facebook-icon'></i>
+                            <span>Login with Facebook</span>
+                        </a>
+                    </div>
+
+                    <div class="media-options">
+                        <a href="#" class="field google">
+                            <img src="images/google.png" alt="" class="google-img">
+                            <span>Login with Google</span>
+                        </a>
+                    </div>
+
+                </div>
+            </section>
+        </section>
+
+
+    </div>
+
