5fd29a90153d449fea3e40feb31069b2ce3a105a
Lalesh <laleshpaudel7@gmail.com>
2022-09-08 16:03:40 +0545

    about-company section added

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -66,3 +66,14 @@
         </div>
     </div>
 </section>
+
+
+<section id="about-company">
+    <div class="flex container">
+        <img src="{{url('frontend/images/3d-medical-figure-with-brain-highlighted.jpg')}}" alt="about company"/>
+        <div>
+            <h2>Lorem ipsum dolor sit amet</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. <br>Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.</p>
+        </div>
+    </div>
+</section>
