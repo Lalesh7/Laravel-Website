cc47f65a3950b49abd7c12399640cac682206607
Lalesh <laleshpaudel7@gmail.com>
2022-09-08 17:14:49 +0545

    footer section added

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,39 @@
+<footer>
+    <div class="flex container">
+        <div class="footer-about">
+            <a id="logo" href="index.html">Bibhuti Solutions</a>
+            <p>Lorem ipsum dolor sit amet</p>
+        </div>
+        <div class="footer-useful-links">
+            <h5>Useful Links</h5>
+            <ul>
+                <li><a href="index.html" class="active">Home</a></li>
+                <li><a href="servives.html"> Services</a></li>
+                <li><a href="team.html">Team</a></li>
+                <li><a href="blog.html">Blog</a></li>
+            </ul>
+        </div>
+        <div class="footer-what-we-do">
+            <h5>What we do</h5>
+            <ul>
+                <li>Development</li>
+                <li>Testing</li>
+                <li>Planning</li>
+                <li>Wealth</li>
+            </ul>
+        </div>
+        <div class="footer-newsletter">
+            <h5>Newsletter</h5>
+            <div id="newsletter-container">
+                <input type="text" placeholder="Name"/>
+                <input type="text" placeholder="Email">
+                <input type="text" placeholder="Subscribe">
+            </div>
+
+        </div>
+    </div>
+    <small>
+        Copyright &copy; 2022 Bibhuti Solutions. All rights reserved.</small>
+</footer>
+
+<script src="{{url('frontend/js/main.js')}}"> </script>
