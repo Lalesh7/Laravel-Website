ec986d40726c070b03acb05a4f865dba0b438958
Lalesh <laleshpaudel7@gmail.com>
2022-09-08 13:08:14 +0545

    index.blade.php file created along with hero head container

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,36 @@
+<div id="header-hero-container">
+    <header>
+        <div class="flex container">
+            <a id="logo" href="index.html">Bibhuti Solutions</a>
+            <nav>
+                <button id="nav-toggle" class="hamburger-menu">
+                    <i class="fas fa-bars"></i>
+                </button>
+
+                <ul id="nav-menu">
+                    <li><a href="index.html" class="active"><b>Home</b></a></li>
+                    <li><a href="about.html"><b>About us</b></a></li>
+                    <li><a href="services.html"><b>Services</b></a></li>
+                    <li><a href="team.html"><b>Team</b></a></li>
+                    <li><a href="blog.html"><b>Blog</b></a></li>
+                    <li><a href="contact.html"><b>Contact us</b></a></li>
+                    <li><a href="login.html"><b>Login</b></a></li>
+                    <li id="close-flyout"><span class="fas fa-window-close"></span></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section id="hero">
+        <div class="fade"></div>
+        <div class="hero-text">
+            <h1>Welcome to Bibhuti Solutions</h1>
+            <p>
+                We offer a well-balanced blend of technological skills, domain knowledge, hands-on experience and effective methodology.
+                Bibhuti Solutions is a registered web development business in Australia founded with an aim to give complete IT solutions.
+                As one of the web development company with a wider array of IT solutions, Bibhuti Solutions helps to solve big challenges that are technically complex and mission-critical.
+        </p>
+            <button style ="margin: 20px;"class="button">Work with us!</button> <button class="button1">Learn</button>
+        </div>
+        </section>
+</div>
