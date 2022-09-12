4fcf198ac714cb8557605a4afbd0c2757555f729
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 12:32:25 +0545

    blogs section added

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -32,3 +32,58 @@
     </div>
 
 </div>
+
+<div class="blogs">
+    <div class="card">
+        <div class="card-header">
+            <img src="images/medium-shot-man-wearing-vr-glasses.jpg" alt="VR">
+        </div>
+        <div class="card-body">
+            <span class="tag tag-teal">Technology</span>
+            <h4> <a href="blog_1.html">Apple's AR/VR Headset may include video content created by Hollywood directors</a></h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptatem asperiores quidem atque autem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias asperiores molestia</p>
+            <div class="card-user">
+                <img src="images/content writer.jpg" alt="user1">
+                <div class="user-info">
+                    <h5>Sabbu</h5>
+                    <small>4 minutes ago</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <img src="images/tourist-sitting-phu-sub-lek-viewpoint-sunset-lopburi-thailand.jpg" alt="Hiking">
+        </div>
+        <div class="card-body">
+            <span class="tag tag-travel">Travel</span>
+            <h4>Lake Powell's water crisis reveals more of one of Arizona's most spectacular canyons</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vero, unde explicabo nesciunt cum maxime Lorem ipsum dolor sit</p>
+            <div class="card-user">
+                <img src="images/hr.jpg" alt="user2">
+                <div class="user-info">
+                    <h5>Harshha</h5>
+                    <small>17 August, 2022</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <div class="card-header">
+            <img src="images/aerial-shot-boat-spiti-river-india.jpg" alt="Lake">
+        </div>
+        <div class="card-body">
+            <span class="tag tag-travel">Travel</span>
+            <h4>Sand surfing the tallest sand dune in North America, diving into Caribbean blue water</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptatem asperiores quidem atque autem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias asperiores molestia</p>
+            <div class="card-user">
+                <img src="images/graphic designer.jpg" alt="user3">
+                <div class="user-info">
+                    <h5>Bhim Vai</h5>
+                    <small>21 September, 2021</small>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
