55fcd10748d5b8ca6ca7a46926c85c37ea55dfbd
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 12:22:39 +0545

    blog.blade.php file created along with hero head container

---
+++ resources/views/frontend/blog.blade.php
@@ -0,0 +1,34 @@
+<div id="blog">
+    <div id="header-hero-container">
+        <header>
+            <div class="flex container">
+                <a id="logo" href="index.html">Bibhuti Solutions</a>
+                <nav>
+                    <button id="nav-toggle" class="hamburger-menu">
+                        <i class="fas fa-bars"></i>
+                    </button>
+
+                    <ul id="nav-menu">
+                        <li><a href="index.html" class="active"><b>Home</b></a></li>
+                        <li><a href="about.html"><b>About us</b></a></li>
+                        <li><a href="services.html"><b>Services</b></a></li>
+                        <li><a href="team.html"><b>Team</b></a></li>
+                        <li><a href="blog.html"><b>Blog</b></a></li>
+                        <li><a href="contact.html"><b>Contact us</b></a></li>
+                        <li><a href="login.html"><b>Login</b></a></li>
+                        <li id="close-flyout"><span class="fas fa-window-close"></span></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <section id="hero">
+            <div class="fade"></div>
+            <div class="hero-text">
+                <h1>Blog</h1>
+                <h2> <a href="index.html" class="tab"> Home </a> / Blog</h2>
+            </div>
+        </section>
+    </div>
+
+</div>
