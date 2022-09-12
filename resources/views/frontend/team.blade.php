4e8d3cc8a2d34b50a726b2a649c2d68ccd2a2107
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 13:23:40 +0545

    main section added

--- resources/views/frontend/team.blade.php
+++ resources/views/frontend/team.blade.php
@@ -31,3 +31,118 @@
         </section>
     </div>
 </div>
+<div class="main">
+    <div class="profile-card">
+        <div class="img">
+            <img src="./images/download.jpg">
+        </div>
+        <div class="caption">
+            <h3>Prajwal Adhikari</h3>
+            <p>Manager</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="profile-card">
+        <div class="img">
+            <img src="./images/graphic designer.jpg">
+        </div>
+        <div class="caption">
+            <h3>Raju Shrestha</h3>
+            <p>Sr. Front-End Developer</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="profile-card">
+        <div class="img">
+            <img src="./images/supervisor.jpg">
+        </div>
+        <div class="caption">
+            <h3>Arun Pandey</h3>
+            <p>Sr. Back-End Developer</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <div class="profile-card">
+        <div class="img">
+            <img src="./images/hr.jpg">
+        </div>
+        <div class="caption">
+            <h3>Sabbu</h3>
+            <p>Front-End Developer</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="profile-card">
+        <div class="img">
+            <img src="./images/content writer.jpg">
+        </div>
+        <div class="caption">
+            <h3>Harshha Sharma</h3>
+            <p>Graphic Designer</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="profile-card">
+        <div class="img">
+            <img src="./images/supervisor.jpg">
+        </div>
+        <div class="caption">
+            <h3>Bhim Vai</h3>
+            <p>Front-End Developer</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="profile-card">
+        <div class="img">
+            <img src="./images/graphic designer.jpg">
+        </div>
+        <div class="caption">
+            <h3>Rashish Thapa</h3>
+            <p>Marketting</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+    <div class="profile-card">
+        <div class="img">
+            <img src="./images/hr.jpg">
+        </div>
+        <div class="caption">
+            <h3>Anu Khadka</h3>
+            <p>Receptionist</p>
+            <div class="social-links">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
