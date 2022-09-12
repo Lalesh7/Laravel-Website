33594d3d34ff120c05819e6936eda18a069b941b
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 14:53:21 +0545

    extends contact.blade.php

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -1,111 +1,119 @@
-<div id="contact_us">
-    <div id="header-hero-container">
-        <header>
-            <div class="flex container">
-                <a id="logo" href="index.html">Bibhuti Solutions</a>
-                <nav>
-                    <button id="nav-toggle" class="hamburger-menu">
-                        <i class="fas fa-bars"></i>
-                    </button>
+@extends('frontend.layouts.main')
 
-                    <ul id="nav-menu">
-                        <li><a href="index.html" class="active"><b>Home</b></a></li>
-                        <li><a href="about.html"><b>About us</b></a></li>
-                        <li><a href="services.html"><b>Services</b></a></li>
-                        <li><a href="team.html"><b>Team</b></a></li>
-                        <li><a href="blog.html"><b>Blog</b></a></li>
-                        <li><a href="contact.html"><b>Contact us</b></a></li>
-                        <li><a href="login.html"><b>Login</b></a></li>
-                        <li id="close-flyout"><span class="fas fa-window-close"></span></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+@section('main-container')
 
-        <section id="hero">
-            <div class="fade"></div>
-            <div class="hero-text">
-                <h1>Contact us</h1>
-                <h2> <a href="index.html" class="tab"> Home </a> / Contact us</h2>
-            </div>
-    </div>
+    <div id="contact_us">
+        <div id="header-hero-container">
+            <header>
+                <div class="flex container">
+                    <a id="logo" href="index.html">Bibhuti Solutions</a>
+                    <nav>
+                        <button id="nav-toggle" class="hamburger-menu">
+                            <i class="fas fa-bars"></i>
+                        </button>
 
-</div>
-<div class="flex_containers">
-    <div class="mail">
-        <div class="icon">
-            <i class="fas fa-envelope"></i>
-        </div>
-        <div class="title">
-            <span>Our Mail</span>
-        </div>
-        <div class="paragraph">
-            <span>admin@webmail.com</span><br>
-            <span>info@webmail.com</span>
-        </div>
-    </div>
-    <div class="site">
-        <div class="icon">
-            <i class="fas fa-globe"></i>
-        </div>
-        <div class="title">
-            <span>Our Site</span>
-        </div>
-        <div class="paragraph">
-            <span>East Gate, Kathmandu 44600</span>
-
-        </div>
-    </div>
-    <div class="call">
-        <div class="icon">
-            <i class="fas fa-phone"></i>
-        </div>
-        <div class="title">
-            <span>Call us</span>
-        </div>
-        <div class="paragraph">
-            <span>9876543210</span><br>
-            <span>8976543241</span>
-        </div>
-    </div>
-</div>
-<div class="any_questions">
-    <section class="contact-us">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-7">
-                    <div class="section-title">
-                        <h2>Any Questions</h2>
-                    </div>
+                        <ul id="nav-menu">
+                            <li><a href="index.html" class="active"><b>Home</b></a></li>
+                            <li><a href="about.html"><b>About us</b></a></li>
+                            <li><a href="services.html"><b>Services</b></a></li>
+                            <li><a href="team.html"><b>Team</b></a></li>
+                            <li><a href="blog.html"><b>Blog</b></a></li>
+                            <li><a href="contact.html"><b>Contact us</b></a></li>
+                            <li><a href="login.html"><b>Login</b></a></li>
+                            <li id="close-flyout"><span class="fas fa-window-close"></span></li>
+                        </ul>
+                    </nav>
                 </div>
+            </header>
+
+            <section id="hero">
+                <div class="fade"></div>
+                <div class="hero-text">
+                    <h1>Contact us</h1>
+                    <h2> <a href="index.html" class="tab"> Home </a> / Contact us</h2>
+                </div>
+        </div>
+
+    </div>
+
+    <div class="flex_containers">
+        <div class="mail">
+            <div class="icon">
+                <i class="fas fa-envelope"></i>
             </div>
-            <div class="row">
-                <div class="col-lg-7">
-                    <form action="/" class="mb-4 mb-lg-0">
-                        <div class="form-row">
-                            <div class="col-md-12 form-group">
-                                <input type="text" name="name" class="form-control" id="name" placeholder="Name" />
-                            </div>
-                            <div class="col-md-12 form-group">
-                                <input type="email" class="form-control" name="email" id="email" placeholder="Email" />
+            <div class="title">
+                <span>Our Mail</span>
+            </div>
+            <div class="paragraph">
+                <span>admin@webmail.com</span><br>
+                <span>info@webmail.com</span>
+            </div>
+        </div>
+        <div class="site">
+            <div class="icon">
+                <i class="fas fa-globe"></i>
+            </div>
+            <div class="title">
+                <span>Our Site</span>
+            </div>
+            <div class="paragraph">
+                <span>East Gate, Kathmandu 44600</span>
+
+            </div>
+        </div>
+        <div class="call">
+            <div class="icon">
+                <i class="fas fa-phone"></i>
+            </div>
+            <div class="title">
+                <span>Call us</span>
+            </div>
+            <div class="paragraph">
+                <span>9876543210</span><br>
+                <span>8976543241</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="any_questions">
+                <section class="contact-us">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-7">
+                                <div class="section-title">
+                                    <h2>Any Questions</h2>
+                                </div>
                             </div>
                         </div>
-                        <div class="form-group">
-                            <input type="text" class="form-control" name="phone" id="phone" placeholder="Phone" />
-                        </div>
-                        <div class="form-group">
-                            <textarea class="form-control" name="message" placeholder="Message"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-light">Send</button>
-                    </form>
-                </div>
+                        <div class="row">
+                            <div class="col-lg-7">
+                                <form action="/" class="mb-4 mb-lg-0">
+                                    <div class="form-row">
+                                        <div class="col-md-12 form-group">
+                                            <input type="text" name="name" class="form-control" id="name" placeholder="Name" />
+                                        </div>
+                                        <div class="col-md-12 form-group">
+                                            <input type="email" class="form-control" name="email" id="email" placeholder="Email" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="phone" id="phone" placeholder="Phone" />
+                                    </div>
+                                    <div class="form-group">
+                                        <textarea class="form-control" name="message" placeholder="Message"></textarea>
+                                    </div>
+                                    <button type="submit" class="btn btn-light">Send</button>
+                                </form>
+                            </div>
 
-                <div class="col-lg-5">
-                    <div class="map">
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.6718825581934!2d85.32603331502867!3d27.69653438279642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1befc84b0eef%3A0xc5af73f0e5ad90ef!2sBibhuti%20Solution%20Pvt.%20Ltd.!5e0!3m2!1sen!2snp!4v1661320814746!5m2!1sen!2snp" width="100%" height="350" frameborder="0" style="border: 0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                            <div class="col-lg-5">
+                                <div class="map">
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.6718825581934!2d85.32603331502867!3d27.69653438279642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1befc84b0eef%3A0xc5af73f0e5ad90ef!2sBibhuti%20Solution%20Pvt.%20Ltd.!5e0!3m2!1sen!2snp!4v1661320814746!5m2!1sen!2snp" width="100%" height="350" frameborder="0" style="border: 0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</div>
+                </section>
+    </div>
+
+@endsection
