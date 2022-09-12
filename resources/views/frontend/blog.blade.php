08c4705a1f9baa46ae0b8deab299c73b2943bfaf
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 14:51:31 +0545

    extends blog.blade.php

--- resources/views/frontend/blog.blade.php
+++ resources/views/frontend/blog.blade.php
@@ -1,101 +1,105 @@
-<div id="blog">
-    <div id="header-hero-container">
-        <header>
-            <div class="flex container">
-                <a id="logo" href="index.html">Bibhuti Solutions</a>
-                <nav>
-                    <button id="nav-toggle" class="hamburger-menu">
-                        <i class="fas fa-bars"></i>
-                    </button>
+@extends('frontend.layouts.main')
 
-                    <ul id="nav-menu">
-                        <li><a href="index.html" class="active"><b>Home</b></a></li>
-                        <li><a href="about.html"><b>About us</b></a></li>
-                        <li><a href="services.html"><b>Services</b></a></li>
-                        <li><a href="team.html"><b>Team</b></a></li>
-                        <li><a href="blog.html"><b>Blog</b></a></li>
-                        <li><a href="contact.html"><b>Contact us</b></a></li>
-                        <li><a href="login.html"><b>Login</b></a></li>
-                        <li id="close-flyout"><span class="fas fa-window-close"></span></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+@section('main-container')
 
-        <section id="hero">
-            <div class="fade"></div>
-            <div class="hero-text">
-                <h1>Blog</h1>
-                <h2> <a href="index.html" class="tab"> Home </a> / Blog</h2>
-            </div>
-        </section>
-    </div>
+    <div id="blog">
+        <div id="header-hero-container">
+            <header>
+                <div class="flex container">
+                    <a id="logo" href="index.html">Bibhuti Solutions</a>
+                    <nav>
+                        <button id="nav-toggle" class="hamburger-menu">
+                            <i class="fas fa-bars"></i>
+                        </button>
 
-</div>
+                        <ul id="nav-menu">
+                            <li><a href="index.html" class="active"><b>Home</b></a></li>
+                            <li><a href="about.html"><b>About us</b></a></li>
+                            <li><a href="services.html"><b>Services</b></a></li>
+                            <li><a href="team.html"><b>Team</b></a></li>
+                            <li><a href="blog.html"><b>Blog</b></a></li>
+                            <li><a href="contact.html"><b>Contact us</b></a></li>
+                            <li><a href="login.html"><b>Login</b></a></li>
+                            <li id="close-flyout"><span class="fas fa-window-close"></span></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
 
-<div class="blogs">
-    <div class="card">
-        <div class="card-header">
-            <img src="images/medium-shot-man-wearing-vr-glasses.jpg" alt="VR">
+            <section id="hero">
+                <div class="fade"></div>
+                <div class="hero-text">
+                    <h1>Blog</h1>
+                    <h2> <a href="index.html" class="tab"> Home </a> / Blog</h2>
+                </div>
+            </section>
         </div>
-        <div class="card-body">
-            <span class="tag tag-teal">Technology</span>
-            <h4> <a href="blog_1.html">Apple's AR/VR Headset may include video content created by Hollywood directors</a></h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptatem asperiores quidem atque autem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias asperiores molestia</p>
-            <div class="card-user">
-                <img src="images/content writer.jpg" alt="user1">
-                <div class="user-info">
-                    <h5>Sabbu</h5>
-                    <small>4 minutes ago</small>
+
+    </div>
+    <div class="blogs">
+        <div class="card">
+            <div class="card-header">
+                <img src="images/medium-shot-man-wearing-vr-glasses.jpg" alt="VR">
+            </div>
+            <div class="card-body">
+                <span class="tag tag-teal">Technology</span>
+                <h4> <a href="blog_1.html">Apple's AR/VR Headset may include video content created by Hollywood directors</a></h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptatem asperiores quidem atque autem? Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias asperiores molestia</p>
+                <div class="card-user">
+                    <img src="images/content writer.jpg" alt="user1">
+                    <div class="user-info">
+                        <h5>Sabbu</h5>
+                        <small>4 minutes ago</small>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="card">
-        <div class="card-header">
-            <img src="images/tourist-sitting-phu-sub-lek-viewpoint-sunset-lopburi-thailand.jpg" alt="Hiking">
-        </div>
-        <div class="card-body">
-            <span class="tag tag-travel">Travel</span>
-            <h4>Lake Powell's water crisis reveals more of one of Arizona's most spectacular canyons</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vero, unde explicabo nesciunt cum maxime Lorem ipsum dolor sit</p>
-            <div class="card-user">
-                <img src="images/hr.jpg" alt="user2">
-                <div class="user-info">
-                    <h5>Harshha</h5>
-                    <small>17 August, 2022</small>
+        <div class="card">
+            <div class="card-header">
+                <img src="images/tourist-sitting-phu-sub-lek-viewpoint-sunset-lopburi-thailand.jpg" alt="Hiking">
+            </div>
+            <div class="card-body">
+                <span class="tag tag-travel">Travel</span>
+                <h4>Lake Powell's water crisis reveals more of one of Arizona's most spectacular canyons</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vero, unde explicabo nesciunt cum maxime Lorem ipsum dolor sit</p>
+                <div class="card-user">
+                    <img src="images/hr.jpg" alt="user2">
+                    <div class="user-info">
+                        <h5>Harshha</h5>
+                        <small>17 August, 2022</small>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="card">
-        <div class="card-header">
-            <img src="images/aerial-shot-boat-spiti-river-india.jpg" alt="Lake">
-        </div>
-        <div class="card-body">
-            <span class="tag tag-travel">Travel</span>
-            <h4>Sand surfing the tallest sand dune in North America, diving into Caribbean blue water</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptatem asperiores quidem atque autem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias asperiores molestia</p>
-            <div class="card-user">
-                <img src="images/graphic designer.jpg" alt="user3">
-                <div class="user-info">
-                    <h5>Bhim Vai</h5>
-                    <small>21 September, 2021</small>
+        <div class="card">
+            <div class="card-header">
+                <img src="images/aerial-shot-boat-spiti-river-india.jpg" alt="Lake">
+            </div>
+            <div class="card-body">
+                <span class="tag tag-travel">Travel</span>
+                <h4>Sand surfing the tallest sand dune in North America, diving into Caribbean blue water</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi voluptatem asperiores quidem atque autem?Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias asperiores molestia</p>
+                <div class="card-user">
+                    <img src="images/graphic designer.jpg" alt="user3">
+                    <div class="user-info">
+                        <h5>Bhim Vai</h5>
+                        <small>21 September, 2021</small>
+                    </div>
                 </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="belief">
+        <div class="content">
+            <div class="box">
+                <h2>Belief for Customers</h2>
+                <p>Lorem ipsum dolor sit</p>
+                <p>Lorem ipsum dolor sit</p>
+                <p>Lorem ipsum dolor sit</p>
+                <button>Get Quote</button>
             </div>
         </div>
     </div>
 
-</div>
-
-<div class="belief">
-    <div class="content">
-        <div class="box">
-            <h2>Belief for Customers</h2>
-            <p>Lorem ipsum dolor sit</p>
-            <p>Lorem ipsum dolor sit</p>
-            <p>Lorem ipsum dolor sit</p>
-            <button>Get Quote</button>
-        </div>
-    </div>
-</div>
+@endsection
