2c3ef3d3f9cc07105e7ef9d6bab1cb2c6f478eac
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 15:12:43 +0545

    extends index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,152 +1,157 @@
-<div id="header-hero-container">
-    <header>
+@extends('frontend.layouts.main')
+
+@section('main-container')
+
+    <div id="header-hero-container">
+        <header>
+            <div class="flex container">
+                <a id="logo" href="index.html">Bibhuti Solutions</a>
+                <nav>
+                    <button id="nav-toggle" class="hamburger-menu">
+                        <i class="fas fa-bars"></i>
+                    </button>
+
+                    <ul id="nav-menu">
+                        <li><a href="index.html" class="active"><b>Home</b></a></li>
+                        <li><a href="about.html"><b>About us</b></a></li>
+                        <li><a href="services.html"><b>Services</b></a></li>
+                        <li><a href="team.html"><b>Team</b></a></li>
+                        <li><a href="blog.html"><b>Blog</b></a></li>
+                        <li><a href="contact.html"><b>Contact us</b></a></li>
+                        <li><a href="login.html"><b>Login</b></a></li>
+                        <li id="close-flyout"><span class="fas fa-window-close"></span></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <section id="hero">
+            <div class="fade"></div>
+            <div class="hero-text">
+                <h1>Welcome to Bibhuti Solutions</h1>
+                <p>
+                    We offer a well-balanced blend of technological skills, domain knowledge, hands-on experience and effective methodology.
+                    Bibhuti Solutions is a registered web development business in Australia founded with an aim to give complete IT solutions.
+                    As one of the web development company with a wider array of IT solutions, Bibhuti Solutions helps to solve big challenges that are technically complex and mission-critical.
+            </p>
+                <button style ="margin: 20px;"class="button">Work with us!</button> <button class="button1">Learn</button>
+            </div>
+            </section>
+    </div>
+
+    <section id="staff">
+        <div class="container">
+
+            <div class="flex">
+                <div class="card">
+                    <img src="{{url('frontend/images/1.jpg')}}" alt="1" />
+                    <h3>Lorem ipsum</h3>
+                    <p>Sed ac arcu tellus. Vestibulum ullamcorper <br> eu sapien lobortis placerat</p>
+                </div>
+
+                <div class="card">
+                    <img src="{{url('frontend/images/2.png')}}" alt="2"/>
+                    <h3>Lorem ipsum</h3>
+                    <p>Sed ac arcu tellus. Vestibulum ullamcorper <br> eu sapien lobortis placerat</p>
+                </div>
+
+                <div class="card">
+                    <img src="{{url('frontend/images/3.png')}}" alt="3"/>
+                    <h3>Lorem ipsum</h3>
+                    <p>Sed ac arcu tellus. Vestibulum ullamcorper <br> eu sapien lobortis placerat</p>
+                </div>
+
+                <div class="card">
+                    <img src="{{url('frontend/images/4.png')}}" alt="4" />
+                    <h3>Lorem ipsum</h3>
+                    <p>Sed ac arcu tellus. Vestibulum ullamcorper <br> eu sapien lobortis placerat</p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section id="about-company">
         <div class="flex container">
-            <a id="logo" href="index.html">Bibhuti Solutions</a>
-            <nav>
-                <button id="nav-toggle" class="hamburger-menu">
-                    <i class="fas fa-bars"></i>
+            <img src="{{url('frontend/images/3d-medical-figure-with-brain-highlighted.jpg')}}" alt="about company"/>
+            <div>
+                <h2>Lorem ipsum dolor sit amet</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. <br>Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.</p>
+            </div>
+        </div>
+    </section>
+
+    <section id="working-steps">
+        <h3>Working Steps We Follow:</h3>
+        <div class="flex-container">
+            <div class="container1">
+                <button type="button" class="icon-button">
+                    <span class="material-icons">forward</span>
+                    <span class="icon-button__badge">1</span>
                 </button>
-
-                <ul id="nav-menu">
-                    <li><a href="index.html" class="active"><b>Home</b></a></li>
-                    <li><a href="about.html"><b>About us</b></a></li>
-                    <li><a href="services.html"><b>Services</b></a></li>
-                    <li><a href="team.html"><b>Team</b></a></li>
-                    <li><a href="blog.html"><b>Blog</b></a></li>
-                    <li><a href="contact.html"><b>Contact us</b></a></li>
-                    <li><a href="login.html"><b>Login</b></a></li>
-                    <li id="close-flyout"><span class="fas fa-window-close"></span></li>
-                </ul>
-            </nav>
-        </div>
-    </header>
-
-    <section id="hero">
-        <div class="fade"></div>
-        <div class="hero-text">
-            <h1>Welcome to Bibhuti Solutions</h1>
-            <p>
-                We offer a well-balanced blend of technological skills, domain knowledge, hands-on experience and effective methodology.
-                Bibhuti Solutions is a registered web development business in Australia founded with an aim to give complete IT solutions.
-                As one of the web development company with a wider array of IT solutions, Bibhuti Solutions helps to solve big challenges that are technically complex and mission-critical.
-        </p>
-            <button style ="margin: 20px;"class="button">Work with us!</button> <button class="button1">Learn</button>
-        </div>
-        </section>
-</div>
-
-<section id="staff">
-    <div class="container">
-
-        <div class="flex">
-            <div class="card">
-                <img src="{{url('frontend/images/1.jpg')}}" alt="1" />
-                <h3>Lorem ipsum</h3>
-                <p>Sed ac arcu tellus. Vestibulum ullamcorper <br> eu sapien lobortis placerat</p>
             </div>
-
-            <div class="card">
-                <img src="{{url('frontend/images/2.png')}}" alt="2"/>
-                <h3>Lorem ipsum</h3>
-                <p>Sed ac arcu tellus. Vestibulum ullamcorper <br> eu sapien lobortis placerat</p>
+            <div class="container2">
+                <button type="button" class="icon-button1">
+                    <span class="material-icons">filter_drama</span>
+                    <span class="icon-button__badge">2</span>
+                </button>
             </div>
-
-            <div class="card">
-                <img src="{{url('frontend/images/3.png')}}" alt="3"/>
-                <h3>Lorem ipsum</h3>
-                <p>Sed ac arcu tellus. Vestibulum ullamcorper <br> eu sapien lobortis placerat</p>
+            <div class="container3">
+                <button type="button" class="icon-button2">
+                    <span class="material-icons">approval</span>
+                    <span class="icon-button__badge">3</span>
+                </button>
             </div>
-
-            <div class="card">
-                <img src="{{url('frontend/images/4.png')}}" alt="4" />
-                <h3>Lorem ipsum</h3>
-                <p>Sed ac arcu tellus. Vestibulum ullamcorper <br> eu sapien lobortis placerat</p>
+            <div class="container4">
+                <button type="button" class="icon-button3">
+                    <span class="material-icons">drive_file_rename_outline</span>
+                    <span class="icon-button__badge">4</span>
+                </button>
             </div>
+        </div>
+    </section>
 
-        </div>
-    </div>
-</section>
-
-
-<section id="about-company">
-    <div class="flex container">
-        <img src="{{url('frontend/images/3d-medical-figure-with-brain-highlighted.jpg')}}" alt="about company"/>
-        <div>
-            <h2>Lorem ipsum dolor sit amet</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. <br>Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.</p>
-        </div>
-    </div>
-</section>
-
-<section id="working-steps">
-    <h3>Working Steps We Follow:</h3>
-    <div class="flex-container">
-        <div class="container1">
-            <button type="button" class="icon-button">
-                <span class="material-icons">forward</span>
-                <span class="icon-button__badge">1</span>
-            </button>
-        </div>
-        <div class="container2">
-            <button type="button" class="icon-button1">
-                <span class="material-icons">filter_drama</span>
-                <span class="icon-button__badge">2</span>
-            </button>
-        </div>
-        <div class="container3">
-            <button type="button" class="icon-button2">
-                <span class="material-icons">approval</span>
-                <span class="icon-button__badge">3</span>
-            </button>
-        </div>
-        <div class="container4">
-            <button type="button" class="icon-button3">
-                <span class="material-icons">drive_file_rename_outline</span>
-                <span class="icon-button__badge">4</span>
-            </button>
-        </div>
-    </div>
-</section>
-
-<section id="skills">
-    <div class="flex-container">
-        <div class="image">
-            <img src="{{url('frontend/images/44.jpg')}}" alt="skills"/>
-        </div>
-        <div class="startup">
-            <h1>Startup Knowledge</h1>
-            <h2>Working Solutions</h2>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit natus, labore aperiam, praesentium temporibus eligendi ex animi reprehenderit, dolores necessitatibus sequi. Quas non odit quia ad atque adipisci numquam sit!</p>
-        </div>
-        <div class="skills_we_count">
-            <div class="skill">
-                <h1>Skill We Count</h1>
-                <div class="skill-name">Leadership</div>
-                <div class="skill-bar"></div>
-                <div class="skill-per" per="80%" style="max-width: 80%"></div>
+    <section id="skills">
+        <div class="flex-container">
+            <div class="image">
+                <img src="{{url('frontend/images/44.jpg')}}" alt="skills"/>
             </div>
-            <div class="skill">
-                <div class="skill-name">Partnership</div>
-                <div class="skill-bar"></div>
-                <div class="skill-per" per="90%" style="max-width: 90%"></div>
+            <div class="startup">
+                <h1>Startup Knowledge</h1>
+                <h2>Working Solutions</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Suscipit natus, labore aperiam, praesentium temporibus eligendi ex animi reprehenderit, dolores necessitatibus sequi. Quas non odit quia ad atque adipisci numquam sit!</p>
             </div>
-            <div class="skill">
-                <div class="skill-name">Development</div>
-                <div class="skill-bar"></div>
-                <div class="skill-per" per="75%" style="max-width: 75%"></div>
+            <div class="skills_we_count">
+                <div class="skill">
+                    <h1>Skill We Count</h1>
+                    <div class="skill-name">Leadership</div>
+                    <div class="skill-bar"></div>
+                    <div class="skill-per" per="80%" style="max-width: 80%"></div>
+                </div>
+                <div class="skill">
+                    <div class="skill-name">Partnership</div>
+                    <div class="skill-bar"></div>
+                    <div class="skill-per" per="90%" style="max-width: 90%"></div>
+                </div>
+                <div class="skill">
+                    <div class="skill-name">Development</div>
+                    <div class="skill-bar"></div>
+                    <div class="skill-per" per="75%" style="max-width: 75%"></div>
+                </div>
             </div>
+        </div>
+
+    </section>
+
+    <div class="start-business">
+        <div class="start_business_h1">
+            <h1>
+                Ready to start Business?
+            </h1>
+        </div>
+        <div class="business_button">
+            <button class="start_button">Grow with us</button>
         </div>
     </div>
 
-</section>
-
-<div class="start-business">
-    <div class="start_business_h1">
-        <h1>
-            Ready to start Business?
-        </h1>
-    </div>
-    <div class="business_button">
-        <button class="start_button">Grow with us</button>
-    </div>
-</div>
+@endsection
