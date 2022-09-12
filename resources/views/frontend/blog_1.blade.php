55e536a0a052d431c23628cc617dd0cad7ee43dc
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 14:48:11 +0545

    extends blog_1.blade.php

--- resources/views/frontend/blog_1.blade.php
+++ resources/views/frontend/blog_1.blade.php
@@ -1,77 +1,83 @@
-<div id="blog_1">
-    <div id="header-hero-container">
-        <header>
-            <div class="flex container">
-                <a id="logo" href="index.html">Bibhuti Solutions</a>
-                <nav>
-                    <button id="nav-toggle" class="hamburger-menu">
-                        <i class="fas fa-bars"></i>
-                    </button>
+@extends('frontend.layouts.main')
 
-                    <ul id="nav-menu">
-                        <li><a href="index.html" class="active"><b>Home</b></a></li>
-                        <li><a href="about.html"><b>About us</b></a></li>
-                        <li><a href="services.html"><b>Services</b></a></li>
-                        <li><a href="team.html"><b>Team</b></a></li>
-                        <li><a href="blog.html"><b>Blog</b></a></li>
-                        <li><a href="contact.html"><b>Contact us</b></a></li>
-                        <li><a href="login.html"><b>Login</b></a></li>
-                        <li id="close-flyout"><span class="fas fa-window-close"></span></li>
-                    </ul>
-                </nav>
-            </div>
-        </header>
+@section('main-container')
 
-        <section id="hero">
-            <div class="fade"></div>
-            <div class="hero-text">
-                <h1>Apple's AR/VR Headset may include video content created by Hollywood directors</h1>
-                <h2> <a href="index.html" class="tab"> Home </a> / Blog</h2>
-            </div>
-        </section>
+    <div id="blog_1">
+        <div id="header-hero-container">
+            <header>
+                <div class="flex container">
+                    <a id="logo" href="index.html">Bibhuti Solutions</a>
+                    <nav>
+                        <button id="nav-toggle" class="hamburger-menu">
+                            <i class="fas fa-bars"></i>
+                        </button>
+
+                        <ul id="nav-menu">
+                            <li><a href="index.html" class="active"><b>Home</b></a></li>
+                            <li><a href="about.html"><b>About us</b></a></li>
+                            <li><a href="services.html"><b>Services</b></a></li>
+                            <li><a href="team.html"><b>Team</b></a></li>
+                            <li><a href="blog.html"><b>Blog</b></a></li>
+                            <li><a href="contact.html"><b>Contact us</b></a></li>
+                            <li><a href="login.html"><b>Login</b></a></li>
+                            <li id="close-flyout"><span class="fas fa-window-close"></span></li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+
+            <section id="hero">
+                <div class="fade"></div>
+                <div class="hero-text">
+                    <h1>Apple's AR/VR Headset may include video content created by Hollywood directors</h1>
+                    <h2> <a href="index.html" class="tab"> Home </a> / Blog</h2>
+                </div>
+            </section>
+        </div>
+
     </div>
-
-</div>
-<section id="blog_container">
-    <div class="blog_details">
-        <div class="card0">
-            <div class="card-header">
-                <img src="images/medium-shot-man-wearing-vr-glasses.jpg" alt="VR">
+    <section id="blog_container">
+        <div class="blog_details">
+            <div class="card0">
+                <div class="card-header">
+                    <img src="images/medium-shot-man-wearing-vr-glasses.jpg" alt="VR">
+                </div>
+                <div class="card-body">
+                    <h4>Apple's AR/VR Headset may include video content created by Hollywood directors</h4>
+                    <small>07 May, 2022</small>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium,
+                     accusantium numquam! Totam ipsum, autem facilis vel pariatur distinctio explicabo modi
+                     quaerat quos natus ducimus harum repudiandae, magni minus tempora nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus facere, aliquid aliquam natus, molestias delectus in voluptatum veritatis maiores exercitationem assumenda cupiditate officiis tempora qui, et commodi velit voluptatem impedit?</p>
+                </div>
             </div>
-            <div class="card-body">
-                <h4>Apple's AR/VR Headset may include video content created by Hollywood directors</h4>
-                <small>07 May, 2022</small>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium,
-                 accusantium numquam! Totam ipsum, autem facilis vel pariatur distinctio explicabo modi
-                 quaerat quos natus ducimus harum repudiandae, magni minus tempora nisi. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus facere, aliquid aliquam natus, molestias delectus in voluptatum veritatis maiores exercitationem assumenda cupiditate officiis tempora qui, et commodi velit voluptatem impedit?</p>
+            <div class="card1">
+                <div class="box">
+                    <div class="search">
+                        <form class="example" style="margin:auto;max-width:300px">
+                            <input type="text" placeholder="Search.." name="search2">
+                            <button type="submit"><i class="fa fa-search"></i></button>
+                          </form>
+                    </div>
+                    <div class="categories">
+                        <h2>Categories</h2>
+                        <ul class="a">
+                            <li><a href="">Technology</a></li>
+                            <li><a href="">Travel</a></li>
+                            <li><a href="">Food</a></li>
+                        </ul>
+                    </div>
+                    <div class="recent_posts">
+                        <h2>Recent Posts</h2>
+                        <ul class="a">
+                            <li><a href="">Apple's AR/VR Headset may include video content</a></li>
+                            <li><a href="">Lake Powell's water crisis</a></li>
+                            <li><a href="">Sand surfing the tallest sand dune</a></li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
         </div>
-        <div class="card1">
-            <div class="box">
-                <div class="search">
-                    <form class="example" style="margin:auto;max-width:300px">
-                        <input type="text" placeholder="Search.." name="search2">
-                        <button type="submit"><i class="fa fa-search"></i></button>
-                      </form>
-                </div>
-                <div class="categories">
-                    <h2>Categories</h2>
-                    <ul class="a">
-                        <li><a href="">Technology</a></li>
-                        <li><a href="">Travel</a></li>
-                        <li><a href="">Food</a></li>
-                    </ul>
-                </div>
-                <div class="recent_posts">
-                    <h2>Recent Posts</h2>
-                    <ul class="a">
-                        <li><a href="">Apple's AR/VR Headset may include video content</a></li>
-                        <li><a href="">Lake Powell's water crisis</a></li>
-                        <li><a href="">Sand surfing the tallest sand dune</a></li>
-                    </ul>
-                </div>
-            </div>
+    </section>
 
-        </div>
-    </div>
-</section>
+@endsection
