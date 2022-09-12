1f8d2522cd8fd77f1d4daa986206d9a0751a463d
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 13:12:12 +0545

    any questions section added

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -69,3 +69,43 @@
         </div>
     </div>
 </div>
+<div class="any_questions">
+    <section class="contact-us">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <div class="section-title">
+                        <h2>Any Questions</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-7">
+                    <form action="/" class="mb-4 mb-lg-0">
+                        <div class="form-row">
+                            <div class="col-md-12 form-group">
+                                <input type="text" name="name" class="form-control" id="name" placeholder="Name" />
+                            </div>
+                            <div class="col-md-12 form-group">
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Email" />
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="phone" id="phone" placeholder="Phone" />
+                        </div>
+                        <div class="form-group">
+                            <textarea class="form-control" name="message" placeholder="Message"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-light">Send</button>
+                    </form>
+                </div>
+
+                <div class="col-lg-5">
+                    <div class="map">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3532.6718825581934!2d85.32603331502867!3d27.69653438279642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1befc84b0eef%3A0xc5af73f0e5ad90ef!2sBibhuti%20Solution%20Pvt.%20Ltd.!5e0!3m2!1sen!2snp!4v1661320814746!5m2!1sen!2snp" width="100%" height="350" frameborder="0" style="border: 0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
