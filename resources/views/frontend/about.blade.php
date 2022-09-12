3201a78aa7b1ce203d92811c9a8265c2127440f3
Lalesh <laleshpaudel7@gmail.com>
2022-09-12 11:55:08 +0545

    company_about section added

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -31,3 +31,12 @@
         </section>
     </div>
 </div>
+<section id="company_about">
+    <div class="flex container">
+        <img src="images/young-attractive-business-colleagues-joyfully-looking-each-other-working-new-project-together-modern-office.jpg" alt="company about"/>
+        <div>
+            <h2>Title</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nisl tincidunt eget nullam non. Quis hendrerit dolor magna eget est lorem ipsum dolor sit. Volutpat odio facilisis mauris sit amet massa. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Mi eget mauris pharetra et. Non tellus orci ac auctor augue. Elit at imperdiet dui accumsan sit. <br>Ornare arcu dui vivamus arcu felis. Egestas integer eget aliquet nibh praesent. In hac habitasse platea dictumst quisque sagittis purus. Pulvinar elementum integer enim neque volutpat ac.</p>
+        </div>
+    </div>
+</section>
